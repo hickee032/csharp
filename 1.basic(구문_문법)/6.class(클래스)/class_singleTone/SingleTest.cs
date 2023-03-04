ead07049e9dd1f363c193a694d3a1f1cdbe41364
Hyun <hickee32@gmail.com>
2023-03-04 23:32:55 +0900

    class

---
+++ 1.basic(구문_문법)/6.class(클래스)/class_singleTone/SingleTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_SingleTone {
+    internal class SingleTest {
+
+        private static SingleTest inst;
+        private int data;
+
+       static Random rd = new Random();
+
+        public SingleTest() {
+
+            data = rd.Next(1,100);
+        }
+        public static SingleTest getInstance() {
+
+            if (inst == null) {
+                inst = new SingleTest();
+            }
+            return inst;        
+        }
+        public int getData() {
+            return data;
+        }
+    }
+}
