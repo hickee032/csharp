ec3597a6a8a6af00ad1fa47678de2aa5ad389fc3
hickee032 <hickee32@gmail.com>
2023-06-11 12:00:00 +0900

    conditional

---
+++ conditional/if_burger.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr_conditional {
+    internal class Conditional {
+        static void Main(string[] args) {
+            
+            for(; ;) {
+                Console.WriteLine();
+                Console.WriteLine("MENU");
+                Console.WriteLine();
+                Console.WriteLine("1.치즈버거");
+                Console.WriteLine("2.치킨버거");
+                Console.WriteLine("3.빅맥버거");
+                Console.WriteLine("Q 종료");
+                Console.WriteLine("선택 : ");
+                string input = Console.ReadLine(); ;
+
+                if (input == "Q" || input == "q") {
+                    Console.WriteLine("종료합니다.");
+                    Environment.Exit(0);
+                }
+                else if(input == "1") {
+                    Console.WriteLine("치즈버거를 선택");
+                }
+                else if (input == "2") {
+                    Console.WriteLine("치킨버거를 선택");
+                }
+                else if (input == "3") {
+                    Console.WriteLine("빅맥버거를 선택");
+                }
+                else {
+                    Console.WriteLine("오류가 발생");
+                }   
+            } 
+            
+            while (true) {
+
+                Console.WriteLine("번호를 선택하세요");
+
+                string input  = Console.ReadLine();
+
+                switch (input) {
+                    case "1":
+                        Console.WriteLine("1번 선택");
+                        break;
+                    case "2":
+                        Console.WriteLine("2번 선택");
+                        break;
+                    case "3":
+                        Console.WriteLine("3번 선택");
+                        break;
+                    case "q":
+                        Console.WriteLine("q번 선택");
+                        break;
+                    case "Q":
+                        Console.WriteLine("Q번 선택");
+                        break;
+                    default:
+                        break;
+                }
+           
+              }
+            
+
+            //점수를 입력받아 점수에 따라 A~F
+
+            //정수 입력
+            int input = int.Parse(Console.ReadLine());
+
+            if (90 < input && input <= 100) {
+                Console.WriteLine("A학점");
+            }
+            else if (80 < input && input <= 89) {
+                Console.WriteLine("B학점");
+            }
+            else if (70 < input && input <= 79) {
+                Console.WriteLine("C학점");
+            }
+            else if (60 < input && input <= 69) {
+                Console.WriteLine("D학점");
+            }
+            else {
+                Console.WriteLine("F학점");
+            }
+
+
+
+        }
+    }
+}
---
+++ conditional/switch_drink.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drink {
+    internal class Drink {
+        static void Main(string[] args) {
+
+
+            Console.WriteLine("--------------");
+            Console.WriteLine("음료수 자판기");
+            Console.WriteLine("--------------");
+            Console.WriteLine("1.콜라 2100원");
+            Console.WriteLine("2.사이다 2000원");
+            Console.WriteLine("3.환타 1500원");
+            Console.WriteLine("4.암바사 1700원");
+            Console.WriteLine("5.웰치스 1800원");
+            Console.WriteLine("--------------");
+            Console.Write("음료수를 선택 >");
+
+            int n = 0;
+            int money = 0;
+            int change = 0;
+
+            try {
+                n = int.Parse(Console.ReadLine());
+            }
+            catch (Exception) {
+
+                Console.WriteLine("잘못된 번호를 입력하였습니다.");
+                Console.WriteLine("프로그램을 종료 합니다.");
+                Environment.Exit(1);
+                throw;
+            }
+
+            switch (n) {
+                case 1:
+                    Console.WriteLine("콜라를 선택하셨습니다.");
+                    Console.WriteLine("음료수 금액을 입력 >");
+                    money = int.Parse(Console.ReadLine());
+                    if (money < 2100) {
+                        Console.WriteLine("구입할수 없습니다.");
+                    }
+                    else {
+                        change = money - 2100;
+                        Console.WriteLine("거스름돈은 {0,0:N0}원 입니다.", change);
+                        Console.WriteLine("거스름돈은 {0:#,0}원 입니다.", change);
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("사이다를 선택하셨습니다.");
+                    Console.WriteLine("음료수 금액을 입력 >");
+                    money = int.Parse(Console.ReadLine());
+                    if (money < 2000) {
+                        Console.WriteLine("구입할수 없습니다.");
+                    }
+                    else {
+                        change = money - 2000;
+                        Console.WriteLine("거스름돈은 {0:C0}원 입니다.", change);
+                    }
+                    break;
+                case 3:
+                    Console.WriteLine("환타를 선택하셨습니다.");
+                    Console.WriteLine("음료수 금액을 입력 >");
+                    money = int.Parse(Console.ReadLine());
+                    if (money < 1500) {
+                        Console.WriteLine("구입할수 없습니다.");
+                    }
+                    else {
+                        change = money - 1500;
+                        Console.WriteLine("거스름돈은 {0:C0}원 입니다.", change);
+                    }
+                    break;
+                case 4:
+                    Console.WriteLine("암바사를 선택하셨습니다.");
+                    Console.WriteLine("음료수 금액을 입력 >");
+                    money = int.Parse(Console.ReadLine());
+                    if (money < 1700) {
+                        Console.WriteLine("구입할수 없습니다.");
+                    }
+                    else {
+                        change = money - 1700;
+                        Console.WriteLine("거스름돈은 {0:C0}원 입니다.", change);
+                    }
+                    break;
+                case 5:
+                    Console.WriteLine("웰치스를 선택하셨습니다.");
+                    Console.WriteLine("음료수 금액을 입력 >");
+                    money = int.Parse(Console.ReadLine());
+                    if (money < 1800) {
+                        Console.WriteLine("구입할수 없습니다.");
+                    }
+                    else {
+                        change = money - 1800;
+                        Console.WriteLine("거스름돈은 {0:C0}원 입니다.", change);
+                    }
+                    break;
+            }
+        }
+    }
+}
