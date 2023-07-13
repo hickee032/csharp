6e0f92cf071f59970cd4c35963994207983cd761
hickee032 <hickee32@gmail.com>
2023-07-13 10:32:00 +0900

    study

---
+++ practice/practice01.cs
@@ -0,0 +1,21 @@
+﻿namespace practice01 {
+    internal class Practice01 {
+        private static void Main(string[] args) {
+            
+        }
+    }
+
+    //객체 지향 내가 표현하고 싶은 걸 클래스로 만들어서 그 클래스로 객체를 만드는 프로그램
+    //클래스(객체)를 기반으로 만들기 때문에 객체를 지향한다
+    //모든요소가 클래스가 된다 
+    //내가 생각한 개념에 사용되어 지기 위한 설계도 
+    class UserPlayer {
+
+    }
+    class Monster {
+
+    }
+    class Pet {
+
+    }
+}
---
+++ practice/practice02.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice02 {
+    internal class practice02 {
+        static void Main(string[] args) {
+
+
+
+        }
+    }
+}
