4d2eedc644d9cc0895de8a5888c276384dde17c3
hickee032 <hickee32@gmail.com>
2023-07-14 10:28:00 +0900

    study

---
+++ practice/problem02.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaekJoon {
+    internal class problem03 {
+
+        static void Main(string[] args) {
+
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine(a);
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(b);
+
+            Console.WriteLine(a+b);
+
+
+        }
+    }
+}
