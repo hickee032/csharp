9ffa3a652073365158229b24279cf03ab8985afe
hickee032 <hickee32@gmail.com>
2023-07-15 09:28:00 +0900

    study

---
+++ practice/problem03.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem03 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            string input = Console.ReadLine();
+            char a = char.Parse(input);
+
+            if (a.Equals('+')) {
+                Console.WriteLine(a);
+            }
+            else {
+
+                Console.WriteLine('X');
+            }
+
+            
+
+
+        }
+    }
+}
