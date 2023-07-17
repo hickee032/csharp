89ac7d0d146dec0858784c8b0543985cb5d93e8e
hickee032 <hickee32@gmail.com>
2023-07-18 07:28:00 +0900

    study

---
+++ practice/inflearn/Do_it/01variable.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        bool boolvar = true;
+        int intvar = 10;
+        float floatvar = 3.4f;
+        char charvar = 'a';
+
+        Console.WriteLine(boolvar);
+        Console.WriteLine(intvar);
+        Console.WriteLine(floatvar);
+        Console.WriteLine(charvar);
+    }
+}
