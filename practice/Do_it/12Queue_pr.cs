578ef0d2d3bf8214004b6aab557984bddf5ee6f5
hickee032 <hickee32@gmail.com>
2023-07-25 12:06:00 +0900

    study

---
+++ practice/Do_it/12Queue_pr.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue_pr {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Queue qu = new Queue();
+
+            //추가
+            qu.Enqueue(1);
+            qu.Enqueue(2);
+            qu.Enqueue(3);
+
+            //제거
+            while (qu.Count>0) {
+                Console.WriteLine(qu.Dequeue());
+            }
+
+        }
+    }
+}
