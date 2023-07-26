a5366975df04d2c4e6d11c15f9f60b8256b712e4
hickee032 <hickee32@gmail.com>
2023-07-26 12:20:00 +0900

    study

---
+++ practice/Do_it/13.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stack_pr {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Stack st = new Stack();
+            //추가
+            st.Push(1);
+            st.Push(2);
+            st.Push(3);
+
+            //삭제
+            while (st.Count>0) {
+                Console.WriteLine(st.Pop());
+                //역순으로 출력 queue 반대
+            }
+
+        }
+    }
+}
---
+++ practice/Do_it/14.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hashtable_pr {
+    internal class Program {
+        static void Main(string[] args) {
+            Hashtable ht = new Hashtable();
+            //키와 값이 필요
+            ht["apple"] = "사과";
+            ht["banana"] = "바나나";
+            ht["orange"] = "오렌지";
+
+            Console.WriteLine(ht["apple"]);
+            Console.WriteLine(ht["banana"]);
+            Console.WriteLine(ht["orange"]);
+
+        }
+    }
+}
