9385135a182b52b61d459684bc8800ff1bc07676
hickee032 <hickee32@gmail.com>
2023-07-30 11:00:00 +0900

    study

---
+++ practice/Do_it/win03/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace FindNumber_Win {
+    partial class Form1 {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent() {
+            this.display = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // display
+            // 
+            this.display.AutoSize = true;
+            this.display.Dock = System.Windows.Forms.DockStyle.Top;
+            this.display.Font = new System.Drawing.Font("굴림", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.display.Location = new System.Drawing.Point(0, 0);
+            this.display.Name = "display";
+            this.display.Size = new System.Drawing.Size(219, 24);
+            this.display.TabIndex = 0;
+            this.display.Text = "게임을 시작합니다";
+            this.display.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 56);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(159, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(202, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "입력";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_start
+            // 
+            this.btn_start.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btn_start.Location = new System.Drawing.Point(0, 97);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(288, 23);
+            this.btn_start.TabIndex = 3;
+            this.btn_start.Text = "게임 시작";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 120);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.display);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label display;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_start;
+    }
+}
+
---
+++ practice/Do_it/win03/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FindNumber_Win {
+    public partial class Form1 : Form {
+
+        private int findnum = 0; 
+        private int findcount = 0;
+        private int inputnum = 0;
+
+        public Form1() {
+            InitializeComponent();
+            var rand = new Random();
+            findnum = rand.Next(1, 21);
+        }
+
+        private void btn_start_Click(object sender, EventArgs e) {
+
+
+            if (inputnum == findnum) {
+                display.Text = "승리";
+            }
+            else {
+
+                if (inputnum > findnum) {
+                    display.Text = "보다 작은수";
+                }
+                else {
+                    display.Text = "보다 큰수";
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+   
+            inputnum = int.Parse(textBox1.Text);
+ 
+        }
+    }
+}
