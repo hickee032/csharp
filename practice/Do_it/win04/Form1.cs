865bb562e821abbc4dc951de782b506e975da8d8
hickee032 <hickee32@gmail.com>
2023-07-31 10:00:00 +0900

    study

---
+++ practice/Do_it/win04/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FindNumber1_Win {
+    public partial class Form1 : Form {
+
+        private int findnum = 0;
+        private int chance = 0;
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btn_start_Click(object sender, EventArgs e) {
+            var rand = new Random();
+            findnum = rand.Next(1, 21);
+            chance = 10;
+            display.Text = "숫자를 입력하세요";
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            int inputNum = Int32.Parse(textBox1.Text);
+
+            if (inputNum == findnum) {
+                display.Text = "승리";
+            }
+            else {
+                chance--;
+                display.Text = chance + " 번 남았습니다";
+            }
+            if (chance <= 0) {
+                display.Text = "실패";
+            }
+
+        }
+    }
+}
