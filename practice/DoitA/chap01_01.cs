44d68c415aabe579599a31115aa1c8c0abc49a07
hickee032 <hickee32@gmail.com>
2023-08-02 10:00:00 +0900

    study

---
+++ practice/DoitA/chap01_01.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoIt_Algorithm {
+    internal class chap01_max3 {
+        static void Main(string[] args) {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("a의 값 = " + a);
+            Console.WriteLine("b의 값 = " + b);
+            Console.WriteLine("c의 값 = " + c);
+
+            int max = a;
+
+            if(b > max) { max= b; }
+            if(c > max) { max= c; }
+
+            Console.WriteLine("Max Number is " + max);
+        }
+    }
+}
