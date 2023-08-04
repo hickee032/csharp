0a7044a056d84119e98d4e10b2d29810fa49bc00
hickee032 <hickee32@gmail.com>
2023-08-04 10:00:00 +0900

    study

---
+++ practice/DoitA/chap01_03.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Max3Func {
+
+    public int Max3Fun(int a, int b, int c) {
+        int max = a;
+        if (b > max) {
+            max = b;
+        }
+        if (c > max) {
+            max = c;
+        }
+
+        return max;
+    }
+
+}
+
+namespace chap01_max3Function {
+
+    internal class chap01_max3Function {
+        static void Main(string[] args) {
+            
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            Max3Func mf = new Max3Func();
+            
+            Console.WriteLine(mf.Max3Fun(a, b, c));
+
+        }
+
+        
+    }
+    
+}
