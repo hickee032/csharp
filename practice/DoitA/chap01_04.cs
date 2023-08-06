d2477a83e01b6545f3378d883f4314f36c590d62
hickee032 <hickee32@gmail.com>
2023-08-06 10:00:00 +0900

    study

---
+++ practice/DoitA/chap01_04.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chap01_judgesign {
+    internal class chap01_judgesign {
+        static void Main(string[] args) {
+
+            int input = int.Parse(Console.ReadLine());
+
+            if (input>0) {
+                Console.WriteLine("양수");
+            }
+            else if (input < 0) {
+                Console.WriteLine("음수");
+            }
+            else {
+                Console.WriteLine("0 입니다");
+            }
+
+
+        }
+    }
+}
