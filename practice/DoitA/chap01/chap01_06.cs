a643fbc0eccf9c76d1d280593a08920161dd90e0
hickee032 <hickee32@gmail.com>
2023-08-08 10:00:00 +0900

    study

---
+++ practice/DoitA/chap01/chap01_06.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chap01_judgeABC2 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            int input = int.Parse(Console.ReadLine());
+
+            if (input == 1) {
+                Console.WriteLine("A");
+            }
+            else if (input == 2) {
+                Console.WriteLine("B");
+            }
+            else if (input ==3) {
+                Console.WriteLine("C");
+            }
+            else if (input == 4) {
+                Console.WriteLine("D"); 
+            }
+            else {
+                Console.WriteLine("F");
+            }
+        }
+    }
+}
