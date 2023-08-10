88ce8b208991e28fbbc17fe3f757ee06339fab37
hickee032 <hickee32@gmail.com>
2023-08-10 10:00:00 +0900

    study

---
+++ practice/DoitA/chap01/chap01_07.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chap01_SumWhile {
+    internal class Program {
+        static void Main(string[] args) {
+            //1부터 n
+            int n = int.Parse(Console.ReadLine());
+            int i = 0;
+            int sum = 0;
+
+            while (i<=n) {
+                sum+= i;
+                i++;
+            }
+            Console.WriteLine("합 : "+sum);
+        }
+    }
+}
