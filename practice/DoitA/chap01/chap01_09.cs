d685b1f51012dddd06850b5e5a2eb287c62bbc1d
hickee032 <hickee32@gmail.com>
2023-08-12 10:00:00 +0900

    study

---
+++ practice/DoitA/chap01/chap01_09.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chap01_SumFor2 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            int n = int.Parse(Console.ReadLine());
+
+            do {
+                Console.WriteLine("n의 값 : "+n);
+                n++;
+            } while (n <=0);
+
+            int sum = 0;
+
+            for (int i = 1; i <= n; i++) {
+                sum += i;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
