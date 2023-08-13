cfc6358c3fa41d226e5f946d6f234dca9c4d0b8a
hickee032 <hickee32@gmail.com>
2023-08-13 10:00:00 +0900

    study

---
+++ practice/DoitA/chap01/chap01_10.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chap01_SumVerboss {
+    internal class chap01_SumVerboss {
+        static void Main(string[] args) {
+            
+            Console.WriteLine("1부터 n까지의 합 ?");
+
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+
+
+            for (int i = 1; i <= n; i++) {
+
+                if (i<n) {
+                    Console.Write(i+" + ");
+                    sum += i;
+                }
+                else {
+                    Console.Write(i + " = ");
+                    sum += i;
+                    
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
