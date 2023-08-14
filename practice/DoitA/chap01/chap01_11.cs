85428fc5de93e52af7a7beebb7e67e8326f16623
hickee032 <hickee32@gmail.com>
2023-08-14 10:00:00 +0900

    study

---
+++ practice/DoitA/chap01/chap01_11.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chap01_SumVerboss2 {
+    internal class chap01_SumVerboss2 {
+        static void Main(string[] args) {
+            
+            int sum = 0;
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < n; i++) {
+                Console.Write(i+" + ");
+                sum += i;
+            }
+
+            Console.Write(n+" = ");
+            sum += n;
+            Console.WriteLine(sum);
+        }
+    }
+}
