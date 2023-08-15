522924883b9570cb73a75618cfe013ad5f5043a1
hickee032 <hickee32@gmail.com>
2023-08-15 10:00:00 +0900

    study

---
+++ practice/DoitA/chap01/chap01_12.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chap01_Alternative1 {
+    internal class chap01_Alternative1 {
+        static void Main(string[] args) {
+
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("+ - 를 번갈아 "+ n +" 만큼 출력");
+
+            for (int i = 0; i < n; i++) {
+                if (i%2 ==0) {
+                    Console.Write("+");
+                }
+                else {
+                    Console.Write("-");
+                }
+            }
+
+
+
+        }
+    }
+}
