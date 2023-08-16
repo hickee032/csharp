42c5c7e7c998db76d378036b6270413747466e99
hickee032 <hickee32@gmail.com>
2023-08-16 10:00:00 +0900

    study

---
+++ practice/DoitA/chap01/chap01_13.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chap01_PrintStar {
+    internal class chap01_PrintStar1 {
+        static void Main(string[] args) {
+
+            int n = int.Parse(Console.ReadLine());
+            int w = int.Parse(Console.ReadLine());
+
+            do {
+                Console.WriteLine("n : " + n);
+            } while (n<=0);
+
+            do {
+                Console.WriteLine("w : "+ w);
+            } while (w<=0||w>n);
+
+            for (int i = 0; i < n; i++) {
+                Console.Write("*");
+                if (i%w == w-1) {
+                    Console.WriteLine();
+                }
+            }
+            if (n%w !=0) {
+                Console.WriteLine();
+            }
+        }
+    }
+}
