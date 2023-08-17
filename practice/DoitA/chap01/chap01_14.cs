7d20617fe0dd355c7a2d95f4d7a13fb8bb74ff3f
hickee032 <hickee32@gmail.com>
2023-08-17 10:00:00 +0900

    study

---
+++ practice/DoitA/chap01/chap01_14.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chap01_PrintStar2 {
+    internal class chap01_TwoDigits {
+        static void Main(string[] args) {
+
+            int n = 0;
+
+            do {
+                n = int.Parse(Console.ReadLine());
+                Console.WriteLine("n : "+n);
+
+            } while (n <10 || n >99);
+
+            Console.WriteLine("n > " + n);
+        }
+    }
+}
