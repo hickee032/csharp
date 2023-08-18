7e2eb6d039ed85e2f4308836a9933f284d5b41a4
hickee032 <hickee32@gmail.com>
2023-08-18 10:00:00 +0900

    study

---
+++ practice/DoitA/chap01/chap01_15.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chap01_Multi99Table {
+    internal class chap01_Multi99Table {
+        static void Main(string[] args) {
+            Console.WriteLine("구구단");
+
+            for (int i = 1; i < 10; i++) {
+                for (int j = 1; j < 10; j++) {
+                    Console.Write(i * j);
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
