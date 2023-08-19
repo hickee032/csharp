a514da070731727c1d1714e72f1a985d0c4882d1
hickee032 <hickee32@gmail.com>
2023-08-19 10:00:00 +0900

    study

---
+++ practice/DoitA/chap01/chap01_16.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chap01_TriangleLB {
+    internal class chap01_TriangleLB {
+        static void Main(string[] args) {
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <=i ; j++) {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
