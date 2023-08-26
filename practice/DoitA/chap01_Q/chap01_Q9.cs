1af9a49ad7ac2a63f3b5e764dfddbf4b8fc664e1
hickee032 <hickee32@gmail.com>
2023-08-26 10:00:00 +0900

    study

---
+++ practice/DoitA/chap01_Q/chap01_Q9.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chap01_Q9 {
+
+    class Sub {
+
+        public int Subof(int a, int b) {
+            int result = 0;
+
+            if (a == b) {
+                Console.WriteLine("a보다 큰값을 입력하세요");
+            }
+            else if (b < a) {
+                Console.WriteLine("a보다 큰값을 입력하세요");
+            }
+            else {
+                result = a - b;
+            }
+            return result;
+        }
+    }
+    internal class chap01_Q9 {
+        static void Main(string[] args) {
+
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            Sub su = new Sub();
+            Console.WriteLine(su.Subof(a, b));
+        }
+    }
+}
