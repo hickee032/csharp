77fb8747c502b4cb66a6beabca4379475588f604
hickee032 <hickee32@gmail.com>
2023-08-28 10:00:00 +0900

    study

---
+++ practice/DoitA/chap01_Q/chap01_Q13.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chap01_Q13 {
+
+    class Square {
+        public void foursquare(int n) {
+
+            for (int i = 1; i <= n; i++) {
+
+                for (int j = 1; j <= n; j++) {
+                    Console.Write("*");
+                }
+                Console.WriteLine(); 
+            }
+        }
+    }
+
+    internal class Program {
+        static void Main(string[] args) {
+
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("개수 : "+a);
+
+            Square sq = new Square();
+            sq.foursquare(a);
+
+        }
+    }
+}
