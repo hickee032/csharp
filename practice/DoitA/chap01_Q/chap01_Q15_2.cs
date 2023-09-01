83bd0f532689491049166c6b5636cb734ad1471e
hickee032 <hickee32@gmail.com>
2023-09-01 10:00:00 +0900

    study

---
+++ practice/DoitA/chap01_Q/chap01_Q15_2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chap01_Q15_2 {
+
+    class Tri {
+
+        public void TriangleLU(int n) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = n; j >0; j--) {
+                    if (i<j) {
+                        Console.Write(" ");
+                    }
+                    else { 
+                        Console.Write("*"); 
+                    }
+                }
+                Console.WriteLine();
+            }
+            
+        }
+    }
+
+    internal class chap01_Q15_2 {
+        static void Main(string[] args) {
+            Tri tr = new Tri();
+            tr.TriangleLU(5);
+
+
+        }
+    }
+}
