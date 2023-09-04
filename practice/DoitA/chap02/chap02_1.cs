20ecdf6a20baf99df1898ac8a9fd859c820ed295
hickee032 <hickee32@gmail.com>
2023-09-04 10:00:00 +0900

    study

---
+++ practice/DoitA/chap02/chap02_1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chap02_intArray {
+    internal class chap02_intArray {
+        static void Main(string[] args) {
+
+            int[] arr = new int[5];
+
+            arr[1] = 37;
+            arr[2] = 51;
+            arr[4] = arr[1]*2;
+
+            for (int i = 0; i < arr.Length; i++) {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
