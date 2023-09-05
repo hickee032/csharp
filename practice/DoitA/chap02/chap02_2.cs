179922385d6c62972674c5d7dd3c5af8ff6d9e33
hickee032 <hickee32@gmail.com>
2023-09-05 10:00:00 +0900

    study

---
+++ practice/DoitA/chap02/chap02_2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chap02_IntArrayInit {
+    internal class chap02_IntArrayInit {
+        static void Main(string[] args) {
+
+            int[] arr = {1, 2,3,4,5};
+
+            for (int i = 0; i < arr.Length; i++) {
+                Console.WriteLine("arr["+i+"] : "+ arr[i]);
+            }
+        }
+    }
+}
