6b61f66224a03d6f1e1cbec13bc073c301802a66
hickee032 <hickee32@gmail.com>
2023-09-09 10:00:00 +0900

    study

---
+++ practice/DoitA/chap02_Q/chap02_Q3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chap02_Q3 {
+
+    class ArrSum {
+        public int ArrSumof(int[] a) {
+            int result = 0;
+
+            for (int i = 0; i < a.Length; i++) {
+                result += a[i];
+            }
+            return result;
+        }
+    }
+
+    internal class Program {
+        static void Main(string[] args) {
+
+            ArrSum sum = new ArrSum();
+            int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            int n = sum.ArrSumof(a);
+            Console.WriteLine(n);
+
+        }
+    }
+}
