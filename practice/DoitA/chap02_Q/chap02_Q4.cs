bdcdd82b3036023ee5b63ac79305f9f04dbaf5c8
hickee032 <hickee32@gmail.com>
2023-09-10 10:00:00 +0900

    study

---
+++ practice/DoitA/chap02_Q/chap02_Q4.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chap02_Q4 {
+    internal class Program {
+
+        static void Copy(int[] a, int[] b) {
+
+            for (int i = 0; i < a.Length; i++) {
+                b[i] = a[i];
+            }
+        }
+
+        static void Main(string[] args) {
+
+            int[] a = { 1, 2, 3, 4, 5, 6, 7 };
+            int[] b = new int[7];
+            Copy(a, b);
+
+            Console.WriteLine(string.Join(", ", b));
+        }
+    }
+}
