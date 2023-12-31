66fd4fd62d6d76f150036a7dc897bb772c733c0b
hickee032 <hickee32@gmail.com>
2023-12-31 19:26:38 +0900

    study

---
+++ practice/sec_02_1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sec_02_1 {
+    internal class Program {
+        public static void Main(string[] args) {
+
+            int num = 10;
+
+            if(num == 10) {
+                Console.WriteLine("10입니다.");
+            }
+            else if(num == 9) {
+                Console.WriteLine("9입니다");
+            }
+            else {
+                Console.WriteLine("10도 9도 아닙니다");
+            }
+
+        }
+    }
+}
